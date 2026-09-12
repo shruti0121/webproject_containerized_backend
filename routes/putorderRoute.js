c4022575a2637dacbc827d816d5b42d4e4a9816a
shruti0121 <shruti0121@server.ca>
2026-09-11 21:41:24 -0700

    Testing the ci/cd deployment

--- routes/putorderRoute.js
+++ routes/putorderRoute.js
@@ -98,6 +98,9 @@ router.post("/", async (req,res) => {
       
       console.log("SNS Message (JSON):");
       console.log(JSON.stringify(message));
+
+
+      
     await snsClient.send(
         new PublishCommand({
           TopicArn: process.env.SNS_TOPIC_ARN,
